6bde2801335f58083bb862b69d4fda387f4eb944
Thu Kha Kyawe <142960264+thukhakyawe@users.noreply.github.com>
2024-09-05 03:30:32 +0000

    learning-terraform-05-09-2024

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,26 +1,31 @@
 resource "random_id" "random" {
   byte_length = 2
+  count       = 2
 }
 
 resource "github_repository" "mtc_repo" {
-  name        = "mtc-repo-${random_id.random.dec}"
+  count       = 2
+  name        = "mtc-repo-${random_id.random[count.index].dec}"
   description = "Code for MTC"
   visibility  = "private"
   auto_init   = true
 }
 
+
 resource "github_repository_file" "readme" {
-  repository = github_repository.mtc_repo.name
-  branch = "main"
-  file = "README.md"
-  content = "# This is for infra developers"
+  count               = 2
+  repository          = github_repository.mtc_repo[count.index].name
+  branch              = "main"
+  file                = "README.md"
+  content             = "# This is for infra developers"
   overwrite_on_create = true
-  }
+}
 
 resource "github_repository_file" "index" {
-  repository = github_repository.mtc_repo.name
-  branch = "main"
-  file = "index.html"
-  content = "# Hello, Terraform!"
+  count               = 2
+  repository          = github_repository.mtc_repo[count.index].name
+  branch              = "main"
+  file                = "index.html"
+  content             = "# Hello, Terraform!"
   overwrite_on_create = true
-  }
+}
