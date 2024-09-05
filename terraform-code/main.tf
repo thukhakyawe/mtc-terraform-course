55a4f545a9f116d6fedaf7f7634e38bc6bd18156
Thu Kha Kyawe <142960264+thukhakyawe@users.noreply.github.com>
2024-09-05 01:48:32 +0000

    learning-terraform-05-09-2024

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -4,3 +4,11 @@ resource "github_repository" "mtc_repo" {
   visibility  = "private"
   auto_init   = true
 }
+
+resource "github_repository_file" "readme" {
+  repository = github_repository.mtc_repo.name
+  branch = "main"
+  file = "README.md"
+  content = "# This repository is for infra developers"
+  overwrite_on_create = true
+  }
