b2fffb2714210b7108b10ae1f50b679dd9e1a108
Thu Kha Kyawe <142960264+thukhakyawe@users.noreply.github.com>
2024-09-10 14:45:56 +0000

    learning-terraform-10-09-2024

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -11,5 +11,23 @@ provider "docker" {
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
+}
 
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+    external = 1880
+  }
+}
+
+output "IP-Address" {
+  value       = docker_container.nodered_container.network_data[0].ip_address
+  description = "The IP Address of the container"
+}
+
+output "container-name" {
+  value = docker_container.nodered_container.name
+  description = "The name of the container"
 }
