c0522375ba86b7a72888d8185343b7b5c1043e50
Thu Kha Kyawe <142960264+thukhakyawe@users.noreply.github.com>
2024-09-14 14:10:20 +0000

    learning-terraform-14-09-2024

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -9,12 +9,29 @@ terraform {
 provider "docker" {
 }
 
+variable "ext_port" {
+  type = number
+  default = 1880  
+}
+
+variable "int_port" {
+  type = number
+  default = 1880  
+}
+
+variable "container_count" {
+  type = number
+  default = 1
+}
+
+
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random" {
-  count   = 2
+  count   = var.container_count
   length  = 4
   special = false
   upper   = false
@@ -23,12 +40,12 @@ resource "random_string" "random" {
 
 
 resource "docker_container" "nodered_container" {
-  count = 2
+  count = var.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.image_id
   ports {
-    internal = 1880
-    # external = 1880
+    internal = var.int_port
+    external = var.ext_port
   }
 }
 
